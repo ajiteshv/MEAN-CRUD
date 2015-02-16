4fdfb8ff1402dee74a51a63c2cfaf35244ab0746
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2015-02-16 16:52:11 +0100

    Gulp setup up and running

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/*
+-----------------------------------------------------------------------------------
+|
+| Gulp configuration
+|
+-----------------------------------------------------------------------------------
+*/
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: 'http://localhost:3000'
+    });
+});
+
+gulp.task('reload', function() {
+    return browserSync.reload();
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./public/sass/*.scss')
+      .pipe(plumber({errorHandler: printError}))
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(minifyCSS())
+      .pipe(gulp.dest('./public/css'))
+      .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('default', ['sass', 'browser-sync', 'reload'], function() {
+    gulp.watch('./public/sass/*', ['sass']);
+    gulp.watch('./public/*', ['reload']);
+    gulp.watch('./public/js/*', ['reload']);
+});
+
+/*
+-----------------------------------------------------------------------------------
+|
+| Error handling
+|
+-----------------------------------------------------------------------------------
+*/
+
+function printError(error) {
+    console.log(error.message);
+    this.emit('end');
+}
